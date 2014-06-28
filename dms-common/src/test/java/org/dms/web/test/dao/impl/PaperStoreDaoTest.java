e535f5841b54a827ecc9e51f6850127e5bc7ac5e
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-06-28 05:47:35 +0000

    sync project "dms-common"

--- dms-common/src/test/java/org/dms/web/test/dao/impl/PaperStoreDaoTest.java
+++ dms-common/src/test/java/org/dms/web/test/dao/impl/PaperStoreDaoTest.java
@@ -2,9 +2,11 @@ package org.dms.web.test.dao.impl;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.dms.web.dao.GenericDao;
+import org.dms.web.entity.Examination;
 import org.dms.web.entity.PaperStatusInfo;
 import org.dms.web.entity.PaperStore;
 import org.dms.web.entity.PaperWorkflow;
@@ -33,11 +35,11 @@ public class PaperStoreDaoTest {
 	public void testAddPaperStore(){		
 		log.info("adding one dummy paper to paper store table ...");
 		long paperNumber = genericDao.getEntity("select count(paperNumber) from PaperStore", null);
-		paperNumber++;	
+		paperNumber++;
 		PaperStore newPaper = new PaperStore(paperNumber, 1
 				, "dummy test paper", "just test data".getBytes()
 				, 1 , new Timestamp(new Date().getTime())
-				, null , null);
+				, null , null , null);
 		genericDao.saveEntity(newPaper);		
 		log.info("saved - " + newPaper);
 		
@@ -48,6 +50,28 @@ public class PaperStoreDaoTest {
 		PaperWorkflow workflow = new PaperWorkflow(newPaper, 0, null, false, null, "dummy test");
 		genericDao.saveEntity(workflow);
 		log.info("saved - "+ workflow);
+		
+		Examination exam = new Examination(newPaper, new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime() + 5000), 0);
+		genericDao.saveEntity(exam);
+		log.info("saved - "+ exam);
+				
+		log.info("Completed ...");
+	}
+	
+	@Test
+	@Ignore
+	public void displayFewPaperDetails(){
+		log.info("Displaying few details papers -");
+		List<Object[]> paperList = genericDao.getList(
+			  " select p.paperStoreId , p.paperTitle, e.examId "
+			+ " from PaperStore p , Examination e "
+			+ " where p.paperStoreId = e.paperStoreRef ", null, 1000);
+		for (Object[] obj : paperList) {
+			long paperId = (long) obj[0];
+			String paperTitle = (String) obj[1];
+			long examId = (long) obj[2];
+			log.info("PaperId = "+paperId +", PaperTitle="+ paperTitle +", ExamId="+ examId);
+		}
 	}
 
 }
