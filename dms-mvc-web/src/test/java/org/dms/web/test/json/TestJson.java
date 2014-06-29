d5cfc484b584c5c0f58a8be317d4d859f423a2ea
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-06-29 11:40:11 +0000

    update project "dms-mvc-web" into "https://gulamkhan.svn.cloudforge.com/myprojects"

--- dms-mvc-web/src/test/java/org/dms/web/test/json/TestJson.java
+++ dms-mvc-web/src/test/java/org/dms/web/test/json/TestJson.java
@@ -7,11 +7,13 @@ import java.util.Map;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestJson {
 	
 	@Test
+	@Ignore
 	public void testJson() throws JsonGenerationException, JsonMappingException, IOException{
 		ObjectMapper mapper = new ObjectMapper();
 		Map<String, Object> jmap = new HashMap<String,Object>();
