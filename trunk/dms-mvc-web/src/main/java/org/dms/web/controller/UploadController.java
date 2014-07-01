76792d6103ac6755db2837cb06d3ee329a0beadc
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-07-01 06:12:21 +0000

    update project "dms-mvc-web" into "https://gulamkhan.svn.cloudforge.com/myprojects"

--- trunk/dms-mvc-web/src/main/java/org/dms/web/controller/UploadController.java
+++ trunk/dms-mvc-web/src/main/java/org/dms/web/controller/UploadController.java
@@ -5,11 +5,15 @@ import java.io.IOException;
 import java.util.Iterator;
 
 import org.apache.log4j.Logger;
+import org.dms.web.bo.PaperService;
 import org.dms.web.constants.ViewConstants;
+import org.dms.web.exception.DmsException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,22 +23,30 @@ public class UploadController {
 
 	private Logger log = Logger.getLogger(this.getClass());
 	
+	@Autowired
+	private PaperService paperService;
+	
+	private String statusmsg = "";
+	
 	@RequestMapping(value="/upload-new-paper", method=RequestMethod.POST)
-	public ModelAndView uploadNewPaper(MultipartHttpServletRequest request){
+	public ModelAndView uploadNewPaper(MultipartHttpServletRequest request
+			, @RequestParam(value="paper-title") String title
+			, @RequestParam(value="uploader-name") String uploaderName){
 		ModelAndView mav = new ModelAndView(ViewConstants.ADMIN_HOME_PAGE.getValue());
 		try {
-			log.info("uploading new paper ...");				
+			log.info("uploading new paper for user "+uploaderName+"...");
 			Iterator<String> itr = request.getFileNames();
 			while (itr.hasNext()) {
 				MultipartFile mpf = request.getFile(itr.next());
 				log.info("recieved file name= "+mpf.getOriginalFilename()+" , file size= "+mpf.getSize()/1024 +" Kb.");
+				statusmsg = paperService.addNewPaperToStore(title, mpf.getBytes(), uploaderName);
 				FileCopyUtils.copy(mpf.getBytes(), new FileOutputStream("/tmp/"+mpf.getOriginalFilename()));
-				log.info("saved to disk.");
-				mav.getModel().put("statusmsg", "Paper uploaded successfully.");
+				log.info("saved to disk at /tmp/"+ mpf.getOriginalFilename());
+				mav.getModel().put("statusmsg", "Paper upload successfull. "+statusmsg);
 				mav.getModel().put("status", "success");
 			}
-		} catch (IOException e) {
-			mav.getModel().put("statusmsg", "Paper upload un-successfull, please try again.");
+		} catch (IOException | DmsException e) {
+			mav.getModel().put("statusmsg", "Paper upload un-successfull, please try again. "+ e.getMessage());
 			mav.getModel().put("status", "error");
 			log.error("Error in uploading new paper.", e);
 		}				
--- trunk/dms-mvc-web/src/main/webapp/WEB-INF/pages/admin/add_paper.jsp
+++ trunk/dms-mvc-web/src/main/webapp/WEB-INF/pages/admin/add_paper.jsp
@@ -2,12 +2,12 @@
 		class="form-horizontal" style="font-size: 10px;">
 	<div class="form-group" style="width: 40%">	
 		<label for="paper-title-id" class="control-label">Paper Title:</label> 
-		<input type="text" name="uname" id="paper-title-id" required="required" class="form-control" />	
+		<input type="text" name="paper-title" id="paper-title-id" required="required" class="form-control" />	
 		
 		<label for="fileupload" class="control-label">Select file:</label> 
 		<input id="fileupload" type="file" name="newfile" class="form-control" >
 				
-		<input type="text" name="username" value="${sUserName}" hidden="false">
+		<input type="text" name="uploader-name" value="${sUserName}" hidden="true">
 		<br>			
 		<input type="submit" value="Upload" class="btn btn-primary btn-sm" />
 	</div>
