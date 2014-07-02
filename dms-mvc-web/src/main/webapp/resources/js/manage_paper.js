2f7523699244f42a266bb39b96fd784a0789b02f
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-07-02 15:34:24 +0000

    update project "dms-mvc-web"

--- dms-mvc-web/src/main/webapp/resources/js/manage_paper.js
+++ dms-mvc-web/src/main/webapp/resources/js/manage_paper.js
@@ -3,3 +3,25 @@ $(function() {
 	$("#papermanagetabs").tabs();
 });
 /* End: To activate constant manage Tabs */
+
+/* Start: To create dropdown of usernames for authorization */
+$(document).ready(function() {
+	var dropdown = '#select-auth-name-id';
+	var uploaderNameTextbox = '#uploader-name-id';
+	$(dropdown).click(function() {
+		window.alert('Fetching list of possible authorizing users. Please be patient while data loads.');
+		var inparam = {};
+		inparam.uploadername = $(uploaderNameTextbox).val();
+		$.getJSON('authorizerlist',function(jd){
+			$(dropdown).empty();
+			$(dropdown).html('<option value="NONE">-- SELECT --</option>');
+			for (var i = 0; i < jd.length; i++) {
+				$(dropdown).append('<option value="'+jd[i]+'">'+jd[i]+'</option>');				
+			}
+		});
+		$(dropdown).empty();
+		console.log('removing all elements');
+	});
+	//$(dropdown).unbind('click');
+});
+/* End: To create dropdown of usernames for authorization */
