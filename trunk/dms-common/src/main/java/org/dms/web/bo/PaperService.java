3be48dd3f8632468845eb45f5ba33610f8e0aa96
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-07-02 10:06:08 +0000

    update project "dms-common"

--- trunk/dms-common/src/main/java/org/dms/web/bo/PaperService.java
+++ trunk/dms-common/src/main/java/org/dms/web/bo/PaperService.java
@@ -4,6 +4,7 @@ import org.dms.web.exception.DmsException;
 
 public interface PaperService {
 	
-	String addNewPaperToStore(String paperTitle, byte[] uploadedData, String uploaderName) throws DmsException;
+	String addNewPaperToStore(String paperTitle, byte[] uploadedData, String uploaderName
+			, boolean assignForauthorization, String assignedAuthorizerName) throws DmsException;
 
 }
