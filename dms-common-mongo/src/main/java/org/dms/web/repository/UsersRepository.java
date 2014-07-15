d63b06cd27af3fd3ea8a72637f0e22670aa6e6c2
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-07-15 18:29:28 +0000

    update

--- dms-common-mongo/src/main/java/org/dms/web/repository/UsersRepository.java
+++ dms-common-mongo/src/main/java/org/dms/web/repository/UsersRepository.java
@@ -1,10 +1,14 @@
 package org.dms.web.repository;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.dms.web.document.Users;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UsersRepository extends MongoRepository<Users, Serializable>{
 	
+	List<Users> findByUsername(String username);
+	
+	Users findOneByUsername(String username);
 }
--- dms-common-mongo/src/test/java/org/dms/web/bulkupload/UploadUsers.java
+++ dms-common-mongo/src/test/java/org/dms/web/bulkupload/UploadUsers.java
@@ -6,36 +6,56 @@ import org.dms.web.document.Users;
 import org.dms.web.repository.UsersRepository;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+import com.mongodb.WriteResult;
 
 public class UploadUsers {
 	
 	private static Logger log = Logger.getLogger(UploadUsers.class);
 	
-	private static AbstractApplicationContext ctx = null;
+	private static AbstractApplicationContext ctx = null;	
 
 	public static void main(String[] args) {
 		log.info("begin uploading users into system ...");
 		ctx = new ClassPathXmlApplicationContext("/spring/applicationContext-mongo.xml"
-												,"/spring/applicationContext-common.xml");
+												,"/spring/applicationContext-core.xml");
+		MongoTemplate mongoTemplate = ctx.getBean(MongoTemplate.class);
 		UsersRepository usersRepository = ctx.getBean(UsersRepository.class);
 		RandomNumberGenerator randomGen = ctx.getBean(RandomNumberGenerator.class);
 		log.info("spring initialized sucessfully ...");
 		
 		Users tmpUser = new Users(randomGen.getRandomLong(), "gs2k", "password", "Ramgarh cantt, India", "gs2k@dms.org", "ADMINISTRATOR");
-		usersRepository.save(tmpUser);
-		log.info("added - " + tmpUser);
+		addOrUpdateUser(tmpUser,mongoTemplate);		
 		tmpUser = new Users(randomGen.getRandomLong(),"BheshanBedi", "bb1298", "Delhi, India",	"bheshan@dms.org", "TEACHER");
-		usersRepository.save(tmpUser);
-		log.info("added - " + tmpUser);
+		addOrUpdateUser(tmpUser,mongoTemplate);
 		tmpUser = new Users(randomGen.getRandomLong(),"AmitShah", "as1298", "Jaipur, India", "amit.shah@dms.org", "PRINCIPAL");
-		usersRepository.save(tmpUser);
-		log.info("added - " + tmpUser);
+		addOrUpdateUser(tmpUser,mongoTemplate);
 		tmpUser = new Users(randomGen.getRandomLong(), "RahulKumar", "rk1298", "Kolkata, India", "rahul.kr@dms.org", "STUDENT");
-		usersRepository.save(tmpUser);
-		log.info("added - " + tmpUser);
+		addOrUpdateUser(tmpUser,mongoTemplate);
 		
-		log.info("Total users in syatem = "+usersRepository.count());
+		log.info("Total users in system = "+usersRepository.count());
 		log.info("completed ...");
+	}
+	
+	private static void addOrUpdateUser(Users toSaveUserDetails, MongoTemplate mongoTemplate ){
+		log.info("Processing record, "+ toSaveUserDetails);
+		Update update = new Update();
+		update.set("userid", toSaveUserDetails.getUserId());
+		update.set("username", toSaveUserDetails.getUsername());
+		update.set("password", toSaveUserDetails.getPassword());
+		update.set("address", toSaveUserDetails.getAddress());
+		update.set("email", toSaveUserDetails.getEmail());
+		update.set("role", toSaveUserDetails.getRole());		
+		WriteResult writeResult = mongoTemplate.upsert(Query.query(Criteria.where("username").is(toSaveUserDetails.getUsername())), update, Users.class);
+		if (writeResult.isUpdateOfExisting()) {
+			log.info("Record updated");
+		} else {
+			log.info("Record inserted with id = "+ writeResult.getUpsertedId());
+		}
 	}
 
 }
