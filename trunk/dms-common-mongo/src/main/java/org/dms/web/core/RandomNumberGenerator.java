bdbc20366e06607fb64474fc0f629500bac4fc12
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-07-15 17:25:27 +0000

    update

---
+++ trunk/dms-common-mongo/src/main/java/org/dms/web/core/RandomNumberGenerator.java
@@ -0,0 +1,25 @@
+package org.dms.web.core;
+
+import java.util.Date;
+import java.util.Random;
+
+public class RandomNumberGenerator {
+	
+	private Random random;
+	
+	public long getRandomLong(){
+		int posRandInt = random.nextInt(Integer.MAX_VALUE );
+		return (new Date().getTime()+ posRandInt) ;
+	}
+
+	public Random getRandom() {
+		return random;
+	}
+	
+	public void setRandom(Random random) {
+		this.random = random;
+	}
+	
+	
+
+}
