0e81e1264e9ecfd9380a3e5abfc2572235d8a67f
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-07-16 18:47:42 +0000

    update

--- dms-common-mongo/src/test/java/org/dms/web/bulkupload/UploadConstants.java
+++ dms-common-mongo/src/test/java/org/dms/web/bulkupload/UploadConstants.java
@@ -7,6 +7,11 @@ import org.dms.web.repository.ConstantsRepository;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+import com.mongodb.WriteResult;
 
 public class UploadConstants {
 
@@ -25,38 +30,48 @@ public class UploadConstants {
 		log.info("spring initialized sucessfully ...");
 		
 		Constants constant = new Constants(randomGen.getRandomInt(),"ACCESS TYPE", 10, "ADMINISTRATOR", 0);
-		constantsRepository.save(constant);
-		log.info("Saved - "+ constant);		
+		addOrUpdateConstants(constant, constantsRepository, mongoTemplate );
 		constant = new Constants(randomGen.getRandomInt(),"ACCESS TYPE", 10, "PRINCIPAL", 0);
-		constantsRepository.save(constant);
-		log.info("Saved - "+ constant);		
+		addOrUpdateConstants(constant, constantsRepository, mongoTemplate );			
 		constant = new Constants(randomGen.getRandomInt(),"ACCESS TYPE", 10, "TEACHER", 0);
-		constantsRepository.save(constant);
-		log.info("Saved - "+ constant);		
+		addOrUpdateConstants(constant, constantsRepository, mongoTemplate );			
 		constant = new Constants(randomGen.getRandomInt(),"ACCESS TYPE", 10, "STUDENT", 0);
-		constantsRepository.save(constant);
-		log.info("Saved - "+ constant);
+		addOrUpdateConstants(constant, constantsRepository, mongoTemplate );
 		
 		constant = new Constants(randomGen.getRandomInt(),"PAPER STATUS", 11, "CREATED", 0);
-		constantsRepository.save(constant);
-		log.info("Saved - "+ constant);
+		addOrUpdateConstants(constant, constantsRepository, mongoTemplate );
 		constant = new Constants(randomGen.getRandomInt(),"PAPER STATUS", 11, "PENDING AUTHORIZATION", 0);
-		constantsRepository.save(constant);
-		log.info("Saved - "+ constant);
+		addOrUpdateConstants(constant, constantsRepository, mongoTemplate );
 		constant = new Constants(randomGen.getRandomInt(),"PAPER STATUS", 11, "PENDING APPROVAL", 0);
-		constantsRepository.save(constant);
-		log.info("Saved - "+ constant);
+		addOrUpdateConstants(constant, constantsRepository, mongoTemplate );
 		constant = new Constants(randomGen.getRandomInt(),"PAPER STATUS", 11, "APPROVED", 0);
-		constantsRepository.save(constant);
-		log.info("Saved - "+ constant);
+		addOrUpdateConstants(constant, constantsRepository, mongoTemplate );
 		constant = new Constants(randomGen.getRandomInt(),"PAPER STATUS", 11, "AUTHORIZED", 0);
-		constantsRepository.save(constant);
-		log.info("Saved - "+ constant);
+		addOrUpdateConstants(constant, constantsRepository, mongoTemplate );
 		constant = new Constants(randomGen.getRandomInt(),"PAPER STATUS", 11, "REJECTED", 0);
-		constantsRepository.save(constant);
-		log.info("Saved - "+ constant);
-				
+		addOrUpdateConstants(constant, constantsRepository, mongoTemplate );
+		
+		log.info("Total constants in system = "+constantsRepository.count());
 		log.info("completed ...");
+	}
+	
+	private static void addOrUpdateConstants(Constants toBeSavedConstant, ConstantsRepository constantsRepository, MongoTemplate mongoTemplate){
+		Update update = new Update();
+		update.set("constantid", toBeSavedConstant.getConstantId());
+		update.set("variablename", toBeSavedConstant.getVariableName());
+		update.set("variableid", toBeSavedConstant.getVariableId());
+		update.set("variablevalue", toBeSavedConstant.getVariableValue());
+		update.set("parentvariableid", toBeSavedConstant.getParentVariableId());
+		WriteResult writeResult = mongoTemplate.upsert(Query.query(
+				Criteria.where("variablename").is(toBeSavedConstant.getVariableName())
+						.and("variablevalue").is(toBeSavedConstant.getVariableValue())
+				), update, Constants.class);
+		if (writeResult.isUpdateOfExisting()) {
+			log.info("Record updated.");
+		} else {
+			log.info("New record inserted with id = "+ writeResult.getUpsertedId());
+		}
+		log.info(constantsRepository.findOneByVariableNameAndVariableValue(toBeSavedConstant.getVariableName(), toBeSavedConstant.getVariableValue()));		
 	}
 
 }
