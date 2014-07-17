2d9d3f7c9f09732e0f94e7bd1c1f09ff84f4a067
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-07-17 20:40:53 +0000

    update

--- dms-common-mongo/src/main/java/org/dms/web/bo/impl/LoginServiceImpl.java
+++ dms-common-mongo/src/main/java/org/dms/web/bo/impl/LoginServiceImpl.java
@@ -1,28 +1,38 @@
 package org.dms.web.bo.impl;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.log4j.Logger;
 import org.dms.web.bo.LoginService;
 import org.dms.web.core.SystemConstants;
 import org.dms.web.document.Users;
+import org.dms.web.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class LoginServiceImpl implements LoginService{
 	
 	private Logger log = Logger.getLogger(this.getClass());
+	
+	@Autowired
+	private UsersRepository usersRepository;
 
 	@Override
 	public Users findUser(String username) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		log.info("searching users records in db with username as "+ username);
+		Users user = usersRepository.findOneByUsername(username);
+		if (user ==null) {
+			log.warn("No Users record found in db with username as "+ username);
+		}
+		return user;
 	}
 
 	@Override
-	public boolean verifyAdminUser(String inputUsername, String inputPassword,
-			String inputRole) throws Exception {
-		// TODO Auto-generated method stub
+	public boolean verifyAdminUser(String inputUsername, String inputPassword, String inputRole) throws Exception {
+		Users actualUser = findUser(inputUsername);		
+		if (actualUser != null && actualUser.getUsername().equals(inputUsername) 
+				&& actualUser.getPassword().equals(inputPassword) 
+				&& actualUser.getRole().equalsIgnoreCase(inputRole)
+				&& actualUser.getRole().equalsIgnoreCase(SystemConstants.ROLE_ADMIN.getValue())) {			
+			return true;
+		}		
 		return false;
 	}
 
