d4148fe6ce419c14f13d4414039653fd75ab25ce
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-07-19 21:59:43 +0000

    update

--- trunk/dms-common-mongo/src/main/java/org/dms/web/bo/impl/PaperServiceImpl.java
+++ trunk/dms-common-mongo/src/main/java/org/dms/web/bo/impl/PaperServiceImpl.java
@@ -13,27 +13,51 @@ import org.dms.web.bo.PaperService;
 import org.dms.web.bo.UserService;
 import org.dms.web.core.RandomNumberGenerator;
 import org.dms.web.core.SystemConstants;
+import org.dms.web.document.Users;
 import org.dms.web.exception.DmsException;
 import org.dms.web.exception.DmsRuntimeException;
+import org.dms.web.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 public class PaperServiceImpl implements PaperService {
 
 	private Logger log = Logger.getLogger(this.getClass());
+	
+	@Autowired
+	private UsersRepository usersRepository;
+	@Autowired
+	private MongoTemplate mongoTemplate;
 
 	@Override
-	public String addNewPaperToStore(String paperTitle, byte[] uploadedData,
-			String uploaderName, boolean assignForauthorization,
+	public String addNewPaperToStore(String paperTitle, byte[] uploadedData, String uploaderName, boolean assignForauthorization,
 			String assignedAuthorizerName) throws DmsException {
-		// TODO Auto-generated method stub
-		return null;
+		log.info("paper title-"+paperTitle);
+		log.info("data-"+uploadedData.length);
+		log.info("uploaderName-"+uploaderName);
+		log.info("assignForauthorization-"+assignForauthorization);
+		log.info("assignedAuthorizerName-"+assignedAuthorizerName);
+		return "upload successfull.";
 	}
 
 	@Override
-	public List<String> getAuthorizerListWhenCreatorIs(String creatorName)
-			throws DmsException {
-		// TODO Auto-generated method stub
-		return null;
+	public List<String> getAuthorizerListWhenCreatorIs(String creatorName) throws DmsException {
+		List<String> tmpList = new ArrayList<String>();
+		if (StringUtils.isEmpty(creatorName)) {
+			throw new DmsException("file creator's name not received.");
+		}
+		Users creator = usersRepository.findOneByUsername(creatorName);
+		if (creator == null) {
+			throw new DmsException(creatorName +" is not a valid system user name.");
+		}
+		for(Users usr :mongoTemplate.find(Query.query(Criteria.where("username").ne(creatorName)
+									.and("role").ne(SystemConstants.ROLE_STUDENT.getValue())), Users.class)){
+			tmpList.add(usr.getUsername());
+		}
+		log.info("found LOVs of possible authorizers name as "+ tmpList);
+		return tmpList;
 	}
 	
 	
