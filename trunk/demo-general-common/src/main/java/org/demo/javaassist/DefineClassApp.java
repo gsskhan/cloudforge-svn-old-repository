89edfafc59b7fecaceaebfe4f28ae60ed0608628
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-08-07 04:57:48 +0000

    update project

--- trunk/demo-general-common/src/main/java/org/demo/javaassist/DefineClassApp.java
+++ trunk/demo-general-common/src/main/java/org/demo/javaassist/DefineClassApp.java
@@ -1,7 +1,10 @@
 package org.demo.javaassist;
 
+import java.lang.reflect.Method;
+
 import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.CtMethod;
 
 import org.apache.log4j.Logger;
 
@@ -15,15 +18,33 @@ public class DefineClassApp {
 		CtClass pointClass = pool.makeClass("Point");
 		// write the class to local hard disk.
 		pointClass.writeFile("/tmp");
-		log.info("Sucessfully written file to disk.");
+		log.info("Sucessfully written file for class "+ pointClass.getName());
 		
 		/* If a CtClass object is converted into a class file by writeFile(), toClass(), or toBytecode()
 		, Javassist freezes that CtClass object. A frozen CtClass can be defrost so that modifications
 		of the class definition will be permitted. */
 		pointClass.defrost();
-		pointClass.setName("Pair");
+		pointClass.setName("NewPoint");
 		pointClass.writeFile("/tmp");
-		log.info("Sucessfully written changes file to disk.");
+		log.info("Sucessfully written changes file to disk for class "+ pointClass.getName());
+		
+		// Add a method to class
+		pointClass.defrost();
+		CtMethod printMethod = CtMethod.make("public int print(int val) { System.out.println(\"tested ok. Value passed = \"+val); return val; }", pointClass);
+		pointClass.addMethod(printMethod);
+		
+		pointClass.writeFile("/tmp");
+		log.info("Sucessfully written file for class "+ pointClass.getName());
+		
+		// Call a method
+		Class<?> newClass = pointClass.toClass();
+		Object obj = newClass.newInstance();
+		Class<?>[] formalParams = new Class[] { int.class };
+		Method newMethod = newClass.getMethod("print", formalParams);
+		Object[] actualParams = new Object[]{new Integer(10)};
+		log.info("Invoking the created class method.");
+		int returnedValue = (int) newMethod.invoke(obj, actualParams);
+		log.info("Returned value from method invoke = " + returnedValue);
 
 		log.info("end ...");
 	}
