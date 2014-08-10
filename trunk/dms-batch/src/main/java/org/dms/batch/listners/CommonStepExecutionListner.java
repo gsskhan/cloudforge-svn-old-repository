88de477bc88d36f8ef005a773b100f95ad548650
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-08-10 18:23:05 +0000

    update project

---
+++ trunk/dms-batch/src/main/java/org/dms/batch/listners/CommonStepExecutionListner.java
@@ -0,0 +1,26 @@
+package org.dms.batch.listners;
+
+import org.apache.log4j.Logger;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+
+
+public class CommonStepExecutionListner implements StepExecutionListener{
+	
+	private Logger log = Logger.getLogger(this.getClass());
+
+	@Override
+	public ExitStatus afterStep(StepExecution se) {
+		ExitStatus status = se.getExitStatus();
+		log.info("Finished step '" + se.getStepName() + "' with ExitCode - "+ status.getExitCode()+ ".");
+		return status;
+	}
+
+	@Override
+	public void beforeStep(StepExecution se) {
+		log.info("Starting step - '" + se.getStepName()+"', with job execution id = " 
+				+ se.getJobExecutionId() +" and id = " + se.getId()+ "." );
+	}
+
+}
