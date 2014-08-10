7174b6d82a7afe8151a4197e421863df38b75f98
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-08-10 18:43:58 +0000

    update project

--- dms-batch/src/main/java/org/dms/batch/listners/CommonStepExecutionListner.java
+++ dms-batch/src/main/java/org/dms/batch/listners/CommonStepExecutionListner.java
@@ -13,14 +13,14 @@ public class CommonStepExecutionListner implements StepExecutionListener{
 	@Override
 	public ExitStatus afterStep(StepExecution se) {
 		ExitStatus status = se.getExitStatus();
-		log.info("Finished step '" + se.getStepName() + "' with ExitCode - "+ status.getExitCode()+ ".");
+		log.info("Finished step [" + se.getStepName() + "] with ExitCode ["+ status.getExitCode()+ "]");
 		return status;
 	}
 
 	@Override
 	public void beforeStep(StepExecution se) {
-		log.info("Starting step - '" + se.getStepName()+"', with job execution id = " 
-				+ se.getJobExecutionId() +" and id = " + se.getId()+ "." );
+		log.info("Starting step [" + se.getStepName()+"] with job execution id [" 
+				+ se.getJobExecutionId() +"] and id [" + se.getId()+ "]" );
 	}
 
 }
