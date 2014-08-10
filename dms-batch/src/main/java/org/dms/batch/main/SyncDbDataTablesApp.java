d89bc5a72a9d03052619dfdb1198fc0437a57486
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-08-10 18:23:05 +0000

    update project

---
+++ dms-batch/src/main/java/org/dms/batch/main/SyncDbDataTablesApp.java
@@ -0,0 +1,40 @@
+package org.dms.batch.main;
+
+import org.apache.log4j.Logger;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SyncDbDataTablesApp {
+	
+	private static Logger log = Logger.getLogger(SyncDbDataTablesApp.class);
+	
+	private static AbstractApplicationContext ctx = null;
+
+	public static void main(String[] args) {
+		log.info("starting to sync both mysql and mongo db datatables contents ...");
+		
+		try {			
+			ctx = new ClassPathXmlApplicationContext("spring/applicationContext-batch-config.xml"
+													,"spring/job/sync-db-job-config.xml");
+			
+			JobLauncher jobLauncher = ctx.getBean(JobLauncher.class);
+			Job job = (Job) ctx.getBean("syncDbJobId");
+			JobParameters jobParameters = new JobParametersBuilder()
+													.addLong("executionStartTime", System.currentTimeMillis())
+													.toJobParameters();
+			JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+			log.info("Final batch status - "+ jobExecution.getStatus());
+			
+			ctx.destroy();
+			log.info("sync completed successfully ...");		
+		} catch (Exception e) {
+			log.error("Error in sync db datatables program.", e);
+		}	
+	}
+
+}
