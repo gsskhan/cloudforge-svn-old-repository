e5b9e61a368c2940de74b2f292f652a375d1a8da
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-08-11 21:32:26 +0000

    update project

--- dms-batch/src/main/java/org/dms/batch/tasklets/MoveDumpToHdfs.java
+++ dms-batch/src/main/java/org/dms/batch/tasklets/MoveDumpToHdfs.java
@@ -1,7 +1,14 @@
 package org.dms.batch.tasklets;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.*;
+import org.apache.hadoop.hdfs.DFSClient;
 import org.apache.log4j.Logger;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
@@ -12,6 +19,10 @@ public class MoveDumpToHdfs implements Tasklet{
 	
 	private Logger log = Logger.getLogger(this.getClass());
 	
+	private String hdfsUrl ="hdfs://localhost:9000";
+	private String destinationFilename = "/user/gsskhan/input/tab_system_users.txt";
+	private String sourceFilename = "/tmp/tab_system_users.txt";
+	
 	@Override
 	public RepeatStatus execute(StepContribution sc, ChunkContext cc) throws Exception {
 		this.copyFromLocalToHdfs();
@@ -21,7 +32,7 @@ public class MoveDumpToHdfs implements Tasklet{
 	private void copyFromLocalToHdfs() {		
 		try {
 			log.info("started: move database dump files to HDFS.");
-			FileSystem hdfs = FileSystem.get(new Configuration());
+			/*FileSystem hdfs = FileSystem.get(new Configuration());
 			Path hdfsHomeDir = hdfs.getHomeDirectory();
 			Path workingDir = hdfs.getWorkingDirectory();
 			log.info("found HDFS home directory as "+ hdfsHomeDir);
@@ -34,7 +45,37 @@ public class MoveDumpToHdfs implements Tasklet{
 			hdfs.mkdirs(newFolderPath);     //Create new Directory
 			Path localFilePath = new Path("/tmp/tab_system_users.txt");
 			Path hdfsFilePath=new Path(newFolderPath+"/tab_system_users.txt");
-			hdfs.copyFromLocalFile(localFilePath, hdfsFilePath);
+			hdfs.copyFromLocalFile(localFilePath, hdfsFilePath);*/
+			
+			Configuration conf = new Configuration();
+			conf.set("fs.defaultFS", this.hdfsUrl);
+			DFSClient client = new DFSClient(new URI(this.hdfsUrl), conf);
+			OutputStream out = null;
+	        InputStream in = null;
+	        try {
+	            if (client.exists(destinationFilename)) {
+	                System.out.println("File already exists in hdfs: " + destinationFilename);
+	                //return;
+	            }
+	            out = new BufferedOutputStream(client.create(destinationFilename, true));
+	            in = new BufferedInputStream(new FileInputStream(sourceFilename));
+	            byte[] buffer = new byte[1024];
+	 
+	            int len = 0;
+	            while ((len = in.read(buffer)) > 0) {
+	                out.write(buffer, 0, len);
+	            }
+	        } finally {
+	            if (client != null) {
+	                client.close();
+	            }
+	            if (in != null) {
+	                in.close();
+	            }
+	            if (out != null) {
+	                out.close();
+	            }
+	        }
 			log.info("finished: moving database dump files to HDFS.");
 		} catch (Exception e) {
 			log.error("error: moving database dump files to HDFS.",e);
