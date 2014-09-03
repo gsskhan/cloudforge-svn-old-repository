6476ff1be1e94dcef4c6ccdeec5fde54ca43f352
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-09-03 11:32:54 +0000

    update project

--- trunk/dms-batch/src/main/resources/scripts/mongo_dms_db_dump.bat
+++ trunk/dms-batch/src/main/resources/scripts/mongo_dms_db_dump.bat
@@ -1,1 +1,12 @@
-mongodump -db dms
+
+echo 'creating dump files'
+C:/mongodb/bin/mongodump --host localhost --port 27017 -db dms --out /tmp/
+
+echo 'archiving and compressing dump files'
+cd /tmp
+"C:/Program Files (x86)/7-Zip/7z.exe" a -tzip dms_mongo_dump.zip dms
+
+echo 'cleaned up disk space'
+echo y| rmdir /S dms
+
+exit
--- trunk/dms-batch/src/main/resources/scripts/mongo_dms_db_dump.sh
+++ trunk/dms-batch/src/main/resources/scripts/mongo_dms_db_dump.sh
@@ -5,7 +5,7 @@ mongodump --host localhost --port 27017 -db dms --out /tmp/
 
 echo 'archiving and compressing dump files'
 cd /tmp
-tar -zcvf dms.tar.gz dms
+tar -zcvf dms_mongo_dump.tar.gz dms
 
 echo 'cleaned up disk space'
 rm -r /tmp/dms
