27703be9a50d02bb15b1d9ec00029c10f38f62e8
gsskhan <gsskhan@8e44e38e-b078-47d7-864b-23a66c188c66>
2014-09-29 20:55:07 +0000

    update project

---
+++ demo-python-common/src/mongo/test_connect_mongodb.py
@@ -0,0 +1,22 @@
+'''
+Created on 30-Sep-2014
+
+@author: gsskhan
+'''
+
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+
+db = client.dms
+collection = db.constants
+
+print 'First record:'
+print collection.find_one()
+
+# find all documents
+print 'All records:'
+results = collection.find()
+for record in results:
+    print record
